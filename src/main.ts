be0c93013ff00a6350db0ce6649e5d7c1ac36fd0
Oleh Vorobets <olehcooperation@gmail.com>
2025-03-09 23:40:18 +0200

    Changed cors configuration

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ async function bootstrap() {
   app.enableCors({
     origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['set-cookie'],
   });
 
